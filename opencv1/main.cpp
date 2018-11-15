6b49b00bb4eaaa3ebed265dc50a0d5abd0564e87
Jayyoung Yang <jay_y1711@hotmail.com>
2018-11-16 00:30:26 +0900

    실행이 안되던 오류 수정

--- opencv1/main.cpp
+++ opencv1/main.cpp
@@ -116,56 +116,57 @@ void processImages(char* fistFrameFilename) {
         //error in opening the first image
         cerr << "Unable to open first image frame: " << fistFrameFilename <<   endl;
         exit(EXIT_FAILURE);
+    }
+    
+    //current image filename
+    string fn(fistFrameFilename);
+    //read input data. ESC or 'q' for quitting
+    while( (char)keyboard != 'q' && (char)keyboard != 27 ){
+        //update the background model
+        pMOG2->apply(frame, fgMaskMOG2,-1);
+        //get the frame number and write it on the current frame
+        size_t index = fn.find_last_of("/");
+        if(index == string::npos) {
+            index = fn.find_last_of("\\");
+        }
+        size_t index2 = fn.find_last_of(".");
+        string prefix = fn.substr(0,index+1);
+        string suffix = fn.substr(index2);
+        string frameNumberString = fn.substr(index+1, index2-index-1);
+        istringstream iss(frameNumberString);
+        int frameNumber = 0;
+        iss >> frameNumber;
+        rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
+                  cv::Scalar(255,255,255), -1);
+        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
+                FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
+        //show the current frame and the fg masks
+        imshow("Frame", frame);
+        morphOps(fgMaskMOG2);
+        imshow("FG Mask MOG2 ", fgMaskMOG2);
+        //get the input from the keyboard
+        keyboard = waitKey(1);
+        //search for the next image in the sequence
+        ostringstream oss;
+        oss << (frameNumber + 1);
+        string nextFrameNumberString = oss.str();
+        string nextFrameFilename = prefix + nextFrameNumberString + suffix;
+        //read the next frame
+        frame = imread(nextFrameFilename);
+        if(frame.empty()){
+            //error in opening the next image in the sequence
+            cerr << "Unable to open image frame: " << nextFrameFilename << endl;
+            exit(EXIT_FAILURE);
+        }
+        //update the path of the current frame
+        fn.assign(nextFrameFilename);
         
-        //current image filename
-        string fn(fistFrameFilename);
-        //read input data. ESC or 'q' for quitting
-        while( (char)keyboard != 'q' && (char)keyboard != 27 ){
-            //update the background model
-            pMOG2->apply(frame, fgMaskMOG2,-1);
-            //get the frame number and write it on the current frame
-            size_t index = fn.find_last_of("/");
-            if(index == string::npos) {
-                index = fn.find_last_of("\\");
-            }
-            size_t index2 = fn.find_last_of(".");
-            string prefix = fn.substr(0,index+1);
-            string suffix = fn.substr(index2);
-            string frameNumberString = fn.substr(index+1, index2-index-1);
-            istringstream iss(frameNumberString);
-            int frameNumber = 0;
-            iss >> frameNumber;
-            rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
-                      cv::Scalar(255,255,255), -1);
-            putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-                    FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
-            //show the current frame and the fg masks
-            imshow("Frame", frame);
-            morphOps(fgMaskMOG2);
-            imshow("FG Mask MOG2 ", fgMaskMOG2);
-            //get the input from the keyboard
-            keyboard = waitKey(1);
-            //search for the next image in the sequence
-            ostringstream oss;
-            oss << (frameNumber + 1);
-            string nextFrameNumberString = oss.str();
-            string nextFrameFilename = prefix + nextFrameNumberString + suffix;
-            //read the next frame
-            frame = imread(nextFrameFilename);
-            if(frame.empty()){
-                //error in opening the next image in the sequence
-                cerr << "Unable to open image frame: " << nextFrameFilename << endl;
-                exit(EXIT_FAILURE);
-            }
-            //update the path of the current frame
-            fn.assign(nextFrameFilename);
-            
-            // save subtracted images
-            string imageToSave =("output_MOG_" +  frameNumberString + ".png");
-            bool saved = imwrite("/Users/jayyoungyang/data/" +imageToSave,fgMaskMOG2);
-            if(!saved) {
-                cerr << "Unable to save " << imageToSave << endl;
-            }
+        // save subtracted images
+        string imageToSave =("output_MOG_" +  frameNumberString + ".png");
+        bool saved = imwrite("/Users/jayyoungyang/data/" +imageToSave,fgMaskMOG2);
+        if(!saved) {
+            cerr << "Unable to save " << imageToSave << endl;
         }
     }
 }
+
